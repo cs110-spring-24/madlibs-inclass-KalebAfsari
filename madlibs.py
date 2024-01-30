ebc2c8f0d25cb0d06aede46631c15de875a7572e
KalebAfsari <blueskaleb12@gmail.com>
2024-01-29 17:23:55 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,20 @@
 # TODO Create your inputs, variables, and add your story here!
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+
+# Add 10 more inputs, update your story along the wa7
+
+name = input("Enter a name:")
+noun2 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+color = input("Enter a color: ")
+location = input("Enter a location: ")
+comment = input("Enter something they will say: ")
+verb2 = input("Enter a verb: ")
+name2 = input("Enter another name: ")
+sport = input("Enter a sport: ")
+sound = input("Enter a sound: ")
+
+
+print (f"{noun} was {adjective} while {verb}")
